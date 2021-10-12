1fc3a86d9f22a541c465221dbd6166db84210427
Stefan Bogasiu <stefan.bogasiu@it-teams.com>
2021-10-12 17:33:53 +0300

    last commit

--- src/test/java/starter/search/search_for_city.java
+++ src/test/java/starter/search/search_for_city.java
@@ -4,9 +4,9 @@ import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Open;
 
-public class search_for_new_york {
-    public static Performable the_weather_api_new_york() {
+public class search_for_city {
+    public static Performable the_weather_api_city(String term) {
         return Task.where("{0} opens the weather api home page",
-                Open.browserOn().the(new_york.class));
+        Open.relativeUrl("http://api.weatherstack.com/current?access_key=c101f43cfa610971ac8fc6607b02d8f7&query="+term));
     }
 }
--- src/test/resources/features/search/search_by_keyword.feature
+++ src/test/resources/features/search/search_by_keyword.feature
@@ -5,3 +5,8 @@ Feature: Search by keyword
     When he looks up "New%20York"
     Then he should see information about "New York"
 
+  Scenario: Searching for a term
+    Given Sergey is navigating to weather api homepage
+    When he looks up "Bucuresti"
+    Then he should see information about "Bucuresti"
+
